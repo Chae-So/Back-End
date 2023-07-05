ffd75bf0af7a6e7b084ba27d39ff43a677ac7bde
OOOIOOOIO <polite159@gmail.com>
2023-07-05 22:55:54 +0900

    package test

---
+++ src/main/java/com/contest/chaeso/ChaesoApplication.java
@@ -0,0 +1,13 @@
+package com.contest.chaeso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChaesoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChaesoApplication.class, args);
+    }
+
+}
