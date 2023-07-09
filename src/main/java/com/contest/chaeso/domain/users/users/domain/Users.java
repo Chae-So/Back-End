b31d72ea79714f3d248cd1cfd6a0477439ec9215
OOOIOOOIO <polite159@gmail.com>
2023-07-09 15:58:54 +0900

    feat: restaurant entity 개발

---
+++ src/main/java/com/contest/chaeso/domain/users/users/domain/Users.java
@@ -0,0 +1,29 @@
+package com.contest.chaeso.domain.users.users.domain;
+
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class Users {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
+    private Long userId;
+
+
+    /**
+     * oneToMany
+     *
+     * restaurant bookmark
+     *
+     * restaurant review
+     */
+}
