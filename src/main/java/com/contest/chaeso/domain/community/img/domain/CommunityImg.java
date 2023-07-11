37b2a42642d9d080198b52647f7122dc1270058f
jnk <dipper25@naver.com>
2023-07-11 17:51:11 +0900

    feat: CommunityImg 엔티티 생성

---
+++ src/main/java/com/contest/chaeso/domain/community/img/domain/CommunityImg.java
@@ -0,0 +1,30 @@
+package com.contest.chaeso.domain.community.img.domain;
+
+import com.contest.chaeso.domain.community.community.domain.Community;
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+import static javax.persistence.FetchType.LAZY;
+
+@Entity
+@Builder
+@AllArgsConstructor
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CommunityImg {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "co_img_id")
+    private Long id;
+
+    @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "co_id")
+    private Community community;
+
+    @NotNull
+    @Column(length = 512)
+    private String coImgLink;
+}
