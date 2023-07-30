27796455dd7d6588402556ed5b62a2c278c76ad2
jnk <dipper25@naver.com>
2023-07-31 02:33:43 +0900

    feat: VeganInfo 채식타입 리스트 조회 기능 추가

--- src/main/java/com/contest/chaeso/domain/users/vegan/api/VeganInfoController.java
+++ src/main/java/com/contest/chaeso/domain/users/vegan/api/VeganInfoController.java
@@ -1,14 +1,30 @@
 package com.contest.chaeso.domain.users.vegan.api;
 
 
+import com.contest.chaeso.domain.users.vegan.application.VeganInfoService;
+import com.contest.chaeso.domain.users.vegan.domain.VeganLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
 @Slf4j
 @RequestMapping("/vegan")
 public class VeganInfoController {
+
+    private final VeganInfoService veganInfoService;
+
+    /**
+     * 비건 단계 리스트 출력
+     * @return Map<Long, String>
+     */
+    @GetMapping
+    public Map<Long, String> veganInfo() {
+        return veganInfoService.veganInfoList();
+    }
 }
--- src/main/java/com/contest/chaeso/domain/users/vegan/application/VeganInfoService.java
+++ src/main/java/com/contest/chaeso/domain/users/vegan/application/VeganInfoService.java
@@ -1,11 +1,29 @@
 package com.contest.chaeso.domain.users.vegan.application;
 
+import com.contest.chaeso.domain.users.vegan.api.dto.res.ResponseVeganInfoListDto;
+import com.contest.chaeso.domain.users.vegan.domain.VeganInfo;
+import com.contest.chaeso.domain.users.vegan.domain.VeganLevel;
+import com.contest.chaeso.domain.users.vegan.domain.repository.VeganInfoRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
 public class VeganInfoService {
+
+    private final VeganInfoRepository veganInfoRepository;
+
+    @Transactional(readOnly = true)
+    public Map<Long, String> veganInfoList() {
+        return veganInfoRepository.findAll().stream()
+                .map(vegan -> ResponseVeganInfoListDto.veganInfoList(vegan))
+                .collect(Collectors.toMap(ResponseVeganInfoListDto::getVeganId, ResponseVeganInfoListDto::getVeganLevel));
+    }
 }
