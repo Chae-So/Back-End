b5eecaf6d14ef4ff146d8e3a84acd8e01174656f
jnk <dipper25@naver.com>
2023-08-01 03:12:32 +0900

    fix: 기존 생성자 패턴의 Projection을 QueryProject 형태로 변경

--- src/main/java/com/contest/chaeso/domain/community/review/review/api/dto/res/ResponseCommunityReviewListDto.java
+++ src/main/java/com/contest/chaeso/domain/community/review/review/api/dto/res/ResponseCommunityReviewListDto.java
@@ -1,14 +1,22 @@
 package com.contest.chaeso.domain.community.review.review.api.dto.res;
 
+import com.querydsl.core.annotations.QueryProjection;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor
 public class ResponseCommunityReviewListDto {
 
+    private Long reviewUserId;
     private String reviewerPicture;
     private String reviewerNickname;
-    private String reviewScore;
     private Integer score;
+
+    @QueryProjection
+    public ResponseCommunityReviewListDto(Long reviewUserId, String reviewerPicture, String reviewerNickname, Integer score) {
+        this.reviewUserId = reviewUserId;
+        this.reviewerPicture = reviewerPicture;
+        this.reviewerNickname = reviewerNickname;
+        this.score = score;
+    }
 }
