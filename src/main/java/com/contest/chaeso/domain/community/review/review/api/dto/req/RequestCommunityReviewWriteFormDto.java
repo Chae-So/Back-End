0c989712f4086a35a6e75e885c342831e05222e1
jnk <dipper25@naver.com>
2023-08-01 03:07:20 +0900

    feat: 커뮤니티 댓글 작성 폼 DTO 생성

---
+++ src/main/java/com/contest/chaeso/domain/community/review/review/api/dto/req/RequestCommunityReviewWriteFormDto.java
@@ -0,0 +1,27 @@
+package com.contest.chaeso.domain.community.review.review.api.dto.req;
+
+import com.contest.chaeso.domain.community.community.domain.Community;
+import com.contest.chaeso.domain.community.review.review.domain.CommunityReview;
+import com.contest.chaeso.domain.users.users.domain.Users;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class RequestCommunityReviewWriteFormDto {
+
+    private Long communityId;
+    private String nickname;
+    private String content;
+
+    public CommunityReview toEntity(Users users, Community community) {
+        return CommunityReview.builder()
+                .users(users)
+                .contents(content)
+                .writer(nickname)
+                .community(community)
+                .build();
+    }
+}
