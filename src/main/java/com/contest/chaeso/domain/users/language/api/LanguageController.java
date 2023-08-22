76dcc4bc48c0ffbbb7c25c18beadcdb16989400f
jnk <dipper25@naver.com>
2023-08-23 05:03:51 +0900

    feat: 언어 도메인 리스트 조회 기능 개발

---
+++ src/main/java/com/contest/chaeso/domain/users/language/api/LanguageController.java
@@ -0,0 +1,15 @@
+package com.contest.chaeso.domain.users.language.api;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@Slf4j
+@RequestMapping("/language")
+public class LanguageController {
+
+
+}
