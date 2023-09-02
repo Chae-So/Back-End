0a7e62e2366b40d185d69c2f5c2582d5706021e0
jnk <dipper25@naver.com>
2023-09-03 05:42:49 +0900

    feat: User 도메인 Controller 생성

---
+++ src/main/java/com/contest/chaeso/domain/users/users/api/UserController.java
@@ -0,0 +1,29 @@
+package com.contest.chaeso.domain.users.users.api;
+
+import com.contest.chaeso.domain.users.users.api.dto.req.RequestUserSignUpDto;
+import com.contest.chaeso.domain.users.users.application.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/sign-up")
+    public ResponseEntity signUp(@RequestBody RequestUserSignUpDto userSignUpDto) throws Exception{
+        userService.signUp(userSignUpDto);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @GetMapping("/jwt-test")
+    public String jwtTest() {
+        return "jwtTest 요청 성공";
+    }
+}
