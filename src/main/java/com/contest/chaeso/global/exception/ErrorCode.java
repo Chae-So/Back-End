046dc7b46850e092b05c7bbe593889df1b4675cd
jnk <dipper25@naver.com>
2023-09-08 02:28:42 +0900

    feat: 에러코드 추가

--- src/main/java/com/contest/chaeso/global/exception/ErrorCode.java
+++ src/main/java/com/contest/chaeso/global/exception/ErrorCode.java
@@ -15,7 +15,10 @@ public enum ErrorCode {
     NOT_FOUND_VEGAN_INFO(BAD_REQUEST, "비건 단계 정보가 올바르지 않습니다."),
     NOT_FOUND_COMMUNITY_CATEGORY(BAD_REQUEST, "커뮤니티 카테고리의 정보가 올바르지 않습니다."),
     NOT_FOUND_USER(BAD_REQUEST, "회원 정보가 올바르지 않습니다."),
-    NOT_FOUND_COMMUNITY_POSTS(BAD_REQUEST, "해당 게시글이 존재하지 않습니다.");
+    ALREADY_EXIST_EMAIL(BAD_REQUEST, "이미 존재하는 이메일입니다."),
+    ALREADY_EXIST_NICKNAME(BAD_REQUEST, "이미 존재하는 닉네임입니다."),
+    NOT_FOUND_COMMUNITY_POSTS(BAD_REQUEST, "해당 게시글이 존재하지 않습니다."),
+    NOT_FOUND_LANGUAGE(BAD_REQUEST, "해당 언어가 존재하지 않습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
