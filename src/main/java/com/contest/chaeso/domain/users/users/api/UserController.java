7bd9844a5b9a4a05a77ac0c2bbe14c4cba9c9550
jnk <dipper25@naver.com>
2023-09-08 02:30:28 +0900

    feat: 회원가입 처리 로직 수정

--- src/main/java/com/contest/chaeso/domain/users/users/api/UserController.java
+++ src/main/java/com/contest/chaeso/domain/users/users/api/UserController.java
@@ -17,7 +17,7 @@ public class UserController {
     private final UserService userService;
 
     @PostMapping("/sign-up")
-    public ResponseEntity signUp(@RequestBody RequestUserSignUpDto userSignUpDto) throws Exception{
+    public ResponseEntity signUp(@RequestBody RequestUserSignUpDto userSignUpDto) throws Exception {
         userService.signUp(userSignUpDto);
         return new ResponseEntity(HttpStatus.OK);
     }
