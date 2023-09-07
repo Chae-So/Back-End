1b2cb8da5f949e12f5b05bfb8a0d37870076d9ae
jnk <dipper25@naver.com>
2023-09-08 02:30:54 +0900

    feat: 회원가입 request 데이터 추가

--- src/main/java/com/contest/chaeso/domain/users/users/api/dto/req/RequestUserSignUpDto.java
+++ src/main/java/com/contest/chaeso/domain/users/users/api/dto/req/RequestUserSignUpDto.java
@@ -1,5 +1,7 @@
 package com.contest.chaeso.domain.users.users.api.dto.req;
 
+import com.contest.chaeso.domain.users.language.domain.Language;
+import com.contest.chaeso.domain.users.vegan.domain.VeganLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -10,4 +12,6 @@ public class RequestUserSignUpDto {
     private String email;
     private String pw;
     private String nickname;
+    private Long languageId;
+    private Long veganId;
 }
