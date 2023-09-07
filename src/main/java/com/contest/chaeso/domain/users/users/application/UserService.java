c3f79a28ab82068ab84d7da577fe67cc49041e15
jnk <dipper25@naver.com>
2023-09-08 02:31:41 +0900

    fix: 회원가입 추가 정보 입력 로직 개선

--- src/main/java/com/contest/chaeso/domain/users/users/application/UserService.java
+++ src/main/java/com/contest/chaeso/domain/users/users/application/UserService.java
@@ -1,9 +1,15 @@
 package com.contest.chaeso.domain.users.users.application;
 
+import com.contest.chaeso.domain.users.language.domain.LanguageInfo;
+import com.contest.chaeso.domain.users.language.domain.repository.LanguageRepository;
 import com.contest.chaeso.domain.users.users.api.dto.req.RequestUserSignUpDto;
 import com.contest.chaeso.domain.users.users.domain.Role;
 import com.contest.chaeso.domain.users.users.domain.Users;
 import com.contest.chaeso.domain.users.users.domain.repository.UsersRepository;
+import com.contest.chaeso.domain.users.vegan.domain.VeganInfo;
+import com.contest.chaeso.domain.users.vegan.domain.repository.VeganInfoRepository;
+import com.contest.chaeso.global.exception.CustomException;
+import com.contest.chaeso.global.exception.ErrorCode;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -15,22 +21,29 @@ public class UserService {
 
     private final UsersRepository usersRepository;
     private final PasswordEncoder passwordEncoder;
+    private final LanguageRepository languageRepository;
+    private final VeganInfoRepository veganInfoRepository;
 
     @Transactional
     public void signUp(RequestUserSignUpDto userSignUpDto) throws Exception {
 
         if (usersRepository.findByEmail(userSignUpDto.getEmail()).isPresent()) {
-            throw new Exception("이미 존재하는 이메일입니다.");
+            throw new CustomException(ErrorCode.ALREADY_EXIST_EMAIL);
         }
 
         if (usersRepository.findUsersByNickname(userSignUpDto.getNickname()).isPresent()) {
-            throw new Exception("이미 존재하는 닉네임입니다.");
+            throw new CustomException(ErrorCode.ALREADY_EXIST_NICKNAME);
         }
+
+        LanguageInfo findLanguage = languageRepository.findById(userSignUpDto.getLanguageId()).orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_LANGUAGE));
+        VeganInfo findVegan = veganInfoRepository.findById(userSignUpDto.getVeganId()).orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_VEGAN_INFO));
 
         Users user = Users.builder()
                 .email(userSignUpDto.getEmail())
                 .pw(userSignUpDto.getPw())
                 .nickname(userSignUpDto.getNickname())
+                .languageInfo(findLanguage)
+                .veganInfo(findVegan)
                 .role(Role.USER)
                 .build();
 
