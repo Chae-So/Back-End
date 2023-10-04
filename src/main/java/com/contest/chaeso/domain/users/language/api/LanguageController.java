fc259fd14b237e35d3f48f6b3ac432923f4eee24
jnk <dipper25@naver.com>
2023-10-04 19:17:55 +0900

    feat: Swagger 적용

--- src/main/java/com/contest/chaeso/domain/users/language/api/LanguageController.java
+++ src/main/java/com/contest/chaeso/domain/users/language/api/LanguageController.java
@@ -2,6 +2,10 @@ package com.contest.chaeso.domain.users.language.api;
 
 import com.contest.chaeso.domain.users.language.api.dto.res.ResponseLanguageListDto;
 import com.contest.chaeso.domain.users.language.application.LanguageService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +17,7 @@ import java.util.List;
 @RestController
 @RequiredArgsConstructor
 @Slf4j
+@Api(value = "LanguageController")
 @RequestMapping("/language")
 public class LanguageController {
 
@@ -22,6 +27,11 @@ public class LanguageController {
      * 언어 정보 리스트 출력
      * @return List<ResponseLanguageListDto>
      */
+    @ApiOperation(value = "언어 정보 조회", notes = "회원가입 전 언어정보를 조회합니다.")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "success"),
+            @ApiResponse(code = 404, message = "not found")
+    })
     @GetMapping
     public List<ResponseLanguageListDto> languageInfo() {
         return languageService.languageInfo();
